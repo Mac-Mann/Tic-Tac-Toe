9b7c0f10db07a28738c386f805a85bdac0adadcb
M-Goldman <macalleegoldman7@gmail.com>
2020-05-03 21:53:30 -0400

    Adding all config files, supporting libraries, and project folder layout :relieved:

---
+++ src/components/GameType.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+// tslint:disable-next-line: no-console
+const Ai = () => console.log('Hi');
+export default Ai;
---
+++ src/components/pages/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+  // tslint:disable-next-line: no-unused-expression
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        Hi my name is Mac Goldman. Thanks for checking out my tic-tac-toe app! I
+        had a lot of fun making this, and would love to work with you. Please
+        check out my website Macgoldman.dev and feel free to reach out to me.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
